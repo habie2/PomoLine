f2e96263e5471d4ad3b4bd7f54b6a22bd4bcd51d
habie2 <100472296@alumnos.uc3m.es>
2023-02-06 15:25:24 +0100

    added keyboard testing

--- draft.py
+++ draft.py
@@ -1,16 +1,22 @@
-from time import sleep
-import sys
-import os
+# from time import sleep
+# import sys
+# import os
 
-frase1 = 'Hola me llamo Juan'
-frase2 = 'Hola me llamo Javi'
-print(frase1, end='')
-sys.stdout.flush()
-sleep(1)
-print(frase2)
+# frase1 = 'Hola me llamo Juan'
+# frase2 = 'Hola me llamo Javi'
+# print(frase1, end='')
+# sys.stdout.flush()
+# sleep(1)
+# print(frase2)
 
-# for i in range(100):
-#     sys.stdout.write("\rProgress: {}%".format(i))
-#     sys.stdout.flush()
-#     time.sleep(0.1)
-os.system('cls')
+# # for i in range(100):
+# #     sys.stdout.write("\rProgress: {}%".format(i))
+# #     sys.stdout.flush()
+# #     time.sleep(0.1)
+# os.system('cls')
+import keyboard
+i = 0
+while True:
+    if keyboard.is_pressed('space'):
+        i += 1
+        print('espacsio' + str(i))
