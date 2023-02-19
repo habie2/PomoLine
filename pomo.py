71211b9899f05f2f1b4a7d7b5a61223d8aa35efc
habie2 <100472296@alumnos.uc3m.es>
2023-02-19 23:52:23 +0100

    m

--- pomo.py
+++ pomo.py
@@ -7,6 +7,8 @@ class Pomo():
         self.mins = mins
 
     def run(self):
+        intro = 'Welcome to pomo.\n[r] refresh'
+        print(intro)
         while True:
             self.check_refresh()
             
@@ -16,6 +18,7 @@ class Pomo():
     
     def refresh(self):
         '''Print time.now() - self.start_time'''
+        time_left = 1
         print(datetime.now() - self.star_time)
 
 pomo = Pomo(25)
